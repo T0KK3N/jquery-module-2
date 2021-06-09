6ff2395efb6a615bfa07c52e8524569bf2a7d431
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-06-08 20:51:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jquery-module-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jquery-module-2)
---
+++ index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    var toAdd = $('input[name=itemCompras]').val();
+  });
+});
